d0d2426c2325da5f0c4cd79dcad1ca6104f6b7f0
DhruvalG <dhruval.gv@gmail.com>
2024-09-07 05:55:54 +0530

    fixed errors

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -13,28 +13,28 @@ function Calculator() {
             <div className='output'>{res}</div>
           </div>
           <div>
-            <button value="7" onClick={e => setValue(value + e.target.value)}/>
-            <button value="8" onClick={e => setValue(value + e.target.value)}/>
-            <button value="9" onClick={e => setValue(value + e.target.value)}/>
-            <button value="*" onClick={e => setValue(value + e.target.value)}/>
+            <button value="7" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>7</button>
+            <button value="8" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>8</button>
+            <button value="9" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>9</button>
+            <button value="*" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>*</button>
           </div>
           <div>
-            <button value="4" onClick={e => setValue(value + e.target.value)}/>
-            <button value="5" onClick={e => setValue(value + e.target.value)}/>
-            <button value="6" onClick={e => setValue(value + e.target.value)}/>
-            <button value="+" onClick={e => setValue(value + e.target.value)}/>
+            <button value="4" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>4</button>
+            <button value="5" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>5</button>
+            <button value="6" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>6</button>
+            <button value="+" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>+</button>
           </div>
           <div>
-            <button value="1" onClick={e => setValue(value + e.target.value)}/>
-            <button value="2" onClick={e => setValue(value + e.target.value)}/>
-            <button value="3" onClick={e => setValue(value + e.target.value)}/>
-            <button value="-" onClick={e => setValue(value + e.target.value)}/>
+            <button value="1" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>1</button>
+            <button value="2" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>2</button>
+            <button value="3" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>3</button>
+            <button value="-" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>-</button>
           </div>
           <div>
-            <button value="C" onClick={e => setValue('') + setRes('')}/>
-            <button value="0" onClick={e => setValue(value + e.target.value)}/>
-            <button value="=" className='equal' onClick={e => setRes(value==0? "Error" : eval(value))}/>
-            <button value="/" onClick={e => setValue(value + e.target.value)}/>
+            <button value="C" onClick={e => e.preventDefault() + setValue('') + setRes('')}>C</button>
+            <button value="0" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>0</button>
+            <button value="=" className='equal' onClick={e => e.preventDefault() + setRes(value==0? "Error" : eval(value))}>=</button>
+            <button value="/" onClick={e => e.preventDefault() + setValue(value + e.target.value)}>/</button>
           </div>
         </form>
       </div>
